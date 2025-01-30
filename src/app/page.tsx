6ce3b9fdc294706829190d6e7bb3c4f8a3c0a3b9
usman359 <m.usman095@gmail.com>
2025-01-30 14:21:56 +0500

    commited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Contact from "@/components/Contact/Contact";
 import Hero from "@/components/Hero/Hero";
 import Products from "@/components/Products/Products";
 import Services from "@/components/Services/Services";
-import Statistics from "@/components/Statistics/Statistics";
+// import Statistics from "@/components/Statistics/Statistics";
 import SuccessStories from "@/components/SuccessStories/SuccessStories";
 import Testimonials from "@/components/Testimonials/Testimonials";
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
 
   return (
     <header className="fixed left-0 right-0 top-0 z-50 bg-transparent">
-      <div className="container mx-auto flex items-center justify-between px-6 py-4">
+      <div className="container mx-auto flex items-center justify-between px-[120px] py-4">
         {/* Logo */}
         <Image
           src={"/header/logo.png"}
