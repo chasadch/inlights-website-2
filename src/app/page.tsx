eb7782484f49ffa998940d9c85959f844c5014a3
usman359 <m.usman095@gmail.com>
2025-02-03 17:23:05 +0500

    commited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,25 @@
 import Contact from "@/components/Contact/Contact";
 import Hero from "@/components/Hero/Hero";
-import Portfolio from "@/components/Portfolio/Portfolio";
+// import Portfolio from "@/components/Portfolio/Portfolio";
 import Products from "@/components/Products/Products";
 import Services from "@/components/Services/Services";
 import Statistics from "@/components/Statistics/Statistics";
-// import Statistics from "@/components/Statistics/Statistics";
 import SuccessStories from "@/components/SuccessStories/SuccessStories";
-import Team from "@/components/Team/Team";
+// import Team from "@/components/Team/Team";
 import Testimonials from "@/components/Testimonials/Testimonials";
 
 export default function HomePage() {
   return (
     <div>
       {/* <Portfolio /> */}
-      <Team />
-      {/* <Hero />
+      {/* <Team /> */}
+      <Hero />
       <Statistics />
       <Products />
       <Services />
       <SuccessStories />
       <Testimonials />
-      <Contact /> */}
+      <Contact />
     </div>
   );
 }
