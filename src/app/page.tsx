bb488ce9ba4433029ec3897b15bb9bd1e049f88c
usman359 <m.usman095@gmail.com>
2025-02-04 16:28:03 +0500

    commited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import About from "@/sections/About/About";
+// import About from "@/sections/About/About";
 import Contact from "@/sections/Contact/Contact";
 import Hero from "@/sections/Hero/Hero";
 // import Portfolio from "@/components/Portfolio/Portfolio";
