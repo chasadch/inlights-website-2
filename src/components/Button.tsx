e637431fcbf95326f63600f50e60846f88cb96eb
usman359 <m.usman095@gmail.com>
2025-02-07 12:25:39 +0500

    commited

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,11 @@
-import { MoveUpRight } from "lucide-react";
 import React from "react";
+import { MoveUpRight } from "lucide-react";
 
-// Extend React's button attributes.
+// No need to override onClick if you extend React.ButtonHTMLAttributes
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "gradient" | "outline";
   children: React.ReactNode;
   icon?: boolean;
-  onClick?: () => void;
 }
 
 export default function Button({
@@ -14,6 +13,7 @@ export default function Button({
   children,
   icon = true,
   onClick,
+  ...rest
 }: ButtonProps) {
   const baseClasses =
     "flex items-center justify-center gap-[12px] rounded-[10px] px-[20px] py-[14px] text-size-6 font-medium transition-all duration-300";
@@ -28,6 +28,7 @@ export default function Button({
           ? "border-[3px] border-[#552DDA] bg-gradient-to-b from-[#552DDA] to-[#1D3E80] text-white shadow-shadowCustom hover:border-opacity-100 hover:bg-none"
           : "border-[3px] border-[#552DDA] bg-transparent text-white shadow-[0px_0px_35.7px_rgba(81,68,220,0.97)] hover:bg-[#5144DCF7]"
       }`}
+      {...rest}
     >
       <span>{children}</span>
       {icon && <MoveUpRight width={17} height={17} />}
