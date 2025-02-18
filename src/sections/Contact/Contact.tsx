811cbb84693108fa90b0e96235ed414ccf41a2e0
usman359 <m.usman095@gmail.com>
2025-02-18 15:29:43 +0500

    commited

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -42,7 +42,7 @@ export default function Contact() {
   };
 
   return (
-    <section className="flex flex-col items-center justify-between overflow-hidden bg-black px-[16px] py-[139px] lg:flex-row lg:items-start lg:px-[70px]">
+    <section className="flex flex-col items-center justify-between gap-[66px] overflow-hidden bg-black px-[16px] py-[139px] lg:flex-row lg:items-start lg:px-[70px]">
       {/* Background SVG wrapped in an absolutely positioned container */}
       {/* <div className="absolute inset-0 left-0 top-0 z-10">
         <ContactBackground />
