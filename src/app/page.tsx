52303166d5a2e9b45acc687844aef890563c7e54
usman359 <m.usman095@gmail.com>
2025-02-21 17:27:34 +0500

    commited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import CallToAction from "@/sections/CallToAction/CallToAction";
+import Button from "@/components/Button";
 import Contact from "@/sections/Contact/Contact";
 import Hero from "@/sections/Hero/Hero";
 import Products from "@/sections/Products/Products";
@@ -8,20 +8,49 @@ import Services from "@/sections/Services/Services";
 import Statistics from "@/sections/Statistics/Statistics";
 import SuccessStories from "@/sections/SuccessStories/SuccessStories";
 import Testimonials from "@/sections/Testimonials/Testimonials";
+import { useRouter } from "next/navigation";
 
 export default function HomePage() {
+  const router = useRouter();
   return (
     <>
       <Hero />
       <Statistics />
       <Products />
       <Services />
-      <CallToAction
+      {/* Talent Section (1024 px)*/}
+      <div
+        className="hidden w-full items-center justify-center px-[70px] lg:flex"
+        style={{
+          paddingBottom: "100px",
+          paddingTop: "100px",
+        }}
+      >
+        <div
+          className="relative h-[182px] w-full max-w-screen-xl overflow-hidden rounded-[32px] bg-cover bg-no-repeat lg:h-[255px]"
+          style={{
+            backgroundImage: "url('/services/this_is_engineering.png')",
+          }}
+        >
+          {/* Overlay Content */}
+          <div className="absolute inset-0 flex flex-col items-center justify-center gap-[24px] sm:flex-row sm:justify-between lg:px-[60px]">
+            {/* Text */}
+            <p className="text-size-7 font-normal text-white lg:text-[36px] xl:text-size-2">
+              We Are Looking for Exceptional Talent
+            </p>
+            {/* Button */}
+            <Button onClick={() => router.push("/jobs")} variant="gradient">
+              Join Us
+            </Button>
+          </div>
+        </div>
+      </div>
+      {/* <CallToAction
         mobilePaddingTop="182px"
         mobilePaddingBottom="167px"
         desktopPaddingTop="100px"
         desktopPaddingBottom="100px"
-      />
+      /> */}
 
       <SuccessStories />
       <Testimonials />
