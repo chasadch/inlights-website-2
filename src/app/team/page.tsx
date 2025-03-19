ee90e0a084128861c9144ac65e9fe31ba3783f3b
usman359 <m.usman095@gmail.com>
2025-03-19 11:26:55 +0500

    fixed call to action on the teams page.

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -77,24 +77,26 @@ export default function TeamPage() {
   ];
 
   return (
-    <section className="mx-auto flex max-w-[1440px] flex-col gap-[21px] px-[16px] pt-[92px] text-white lg:gap-[44px] lg:px-[120px] lg:pt-[136px]">
-      {/* Our Team Header */}
-      <div className="flex flex-col items-start lg:gap-[64px]">
-        <h1 className="text-size-3 font-medium lg:text-size-2">Our team</h1>
-      </div>
+    <>
+      <section className="mx-auto flex max-w-[1440px] flex-col gap-[21px] px-[16px] pt-[92px] text-white lg:gap-[44px] lg:px-[120px] lg:pt-[136px]">
+        {/* Our Team Header */}
+        <div className="flex flex-col items-start lg:gap-[64px]">
+          <h1 className="text-size-3 font-medium lg:text-size-2">Our team</h1>
+        </div>
 
-      {/* Team Cards */}
-      <div className="grid grid-cols-2 gap-[13px] sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-2 lg:gap-[74px] xl:grid-cols-3">
-        {teamMembers.map((member, index) => (
-          <TeamCard
-            key={index}
-            image={member.image}
-            name={member.name}
-            role={member.role}
-            linkedInUrl={member.linkedInUrl}
-          />
-        ))}
-      </div>
+        {/* Team Cards */}
+        <div className="grid grid-cols-2 gap-[13px] sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-2 lg:gap-[74px] xl:grid-cols-3">
+          {teamMembers.map((member, index) => (
+            <TeamCard
+              key={index}
+              image={member.image}
+              name={member.name}
+              role={member.role}
+              linkedInUrl={member.linkedInUrl}
+            />
+          ))}
+        </div>
+      </section>
       {/* Talent Section */}
       <CallToAction2
         mobilePaddingTop="156.45px"
@@ -103,6 +105,6 @@ export default function TeamPage() {
         desktopPaddingBottom="172px"
         isBackground={false}
       />
-    </section>
+    </>
   );
 }
